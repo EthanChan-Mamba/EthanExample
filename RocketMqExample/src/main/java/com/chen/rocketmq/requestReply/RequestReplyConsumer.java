47c0a00b332cdb871c18fceb2079c99ddea9ca54
chenyixian <dashu1367198097@163.com>
2022-08-23 14:52:53 +0800

    RocketmqExample 1. 补充事务生产者 2. 新增Request-Reply

---
+++ RocketMqExample/src/main/java/com/chen/rocketmq/requestReply/RequestReplyConsumer.java
@@ -0,0 +1,58 @@
+package com.chen.rocketmq.requestReply;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.utils.MessageUtil;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @author chen
+ * @version 1.0.0
+ * @ClassName RequestReplyConsumer.java
+ * @Description 消费者接受主题消息发送RPC响应。收到响应后需要再做一次生产，使用工具类MessageUtil封装消息后进行响应消息发送
+ * @createTime 2022-08-23 14:36
+ */
+public class RequestReplyConsumer {
+    public static void main(String[] args) throws MQClientException {
+        String producerGroup = "rpc-p";
+        String consumerGroup = "rpc-c";
+        String topic = "RequestTopic";
+
+        DefaultMQProducer replyProducer = new DefaultMQProducer(producerGroup);
+        replyProducer.setNamesrvAddr("127.0.0.1:9876");
+        replyProducer.start();
+
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(consumerGroup);
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        consumer.setPullTimeDelayMillsWhenException(0L);
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                for (MessageExt msg : msgs) {
+                    try {
+                        String replyTo = MessageUtil.getReplyToClient(msg);
+                        byte[] replyContent = "reply message contents.".getBytes();
+                        Message replyMessage = MessageUtil.createReplyMessage(msg, replyContent);
+                        SendResult replyResult = replyProducer.send(replyMessage, 3000);
+                        System.out.printf("reply to %s , %s /n  ", replyTo, replyResult.toString());
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.subscribe(topic, "*");
+        consumer.start();
+    }
+}
---
+++ RocketMqExample/src/main/java/com/chen/rocketmq/requestReply/RequestReplyProducerAsync.java
@@ -0,0 +1,50 @@
+package com.chen.rocketmq.requestReply;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.RequestCallback;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @author chen
+ * @version 1.0.0
+ * @ClassName RequestReplyProducer.java
+ * @Description PRC方式中，生产者也可以使用异步方式发起
+ * @createTime 2022-08-23 14:12
+ */
+public class RequestReplyProducerAsync {
+    public static void main(String[] args) throws MQClientException {
+        String producerGroup = "RPC";
+        String topic = "RequestTopic";
+        // 超时时间
+        long ttl = 3000;
+
+        DefaultMQProducer producer = new DefaultMQProducer(producerGroup);
+        producer.setNamesrvAddr("127.0.0..1:9876");
+        producer.start();
+
+        try {
+            Message message = new Message(topic, "", "Hello World".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            long begin = System.currentTimeMillis();
+            producer.request(message, new RequestCallback() {
+                @Override
+                public void onSuccess(Message message) {
+                    long cost = System.currentTimeMillis() - begin;
+                    System.out.printf("request to <%s> cost: %dms replymessage: %s %n", topic, cost, message);
+
+                }
+
+                @Override
+                public void onException(Throwable throwable) {
+                    System.out.printf("request to <%s> fail.", topic);
+                }
+            }, ttl);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        producer.shutdown();
+
+    }
+}
