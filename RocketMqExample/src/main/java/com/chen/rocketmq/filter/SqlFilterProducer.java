d41589f27553d98b0b508c73fbc57b67db86b206
chenyixian <dashu1367198097@163.com>
2022-08-23 11:01:51 +0800

    RocketmqExample 1. 新增消费sql过滤example

---
+++ RocketMqExample/src/main/java/com/chen/rocketmq/filter/SqlFilterProducer.java
@@ -0,0 +1,34 @@
+package com.chen.rocketmq.filter;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @author chen
+ * @version 1.0.0
+ * @ClassName SqlFilterProducer.java
+ * @Description sql过滤 putUserProperty来设置消息的属性
+ * @createTime 2022-08-22 17:17
+ */
+public class SqlFilterProducer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("SqlFilterProducer");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        producer.start();
+
+        String[] tags = {"TagA", "TagB", "TagC"};
+        for (int i = 0; i < 10; i++) {
+            Message msg = new Message("SqlFilterTest"
+                    , tags[i % tags.length]
+                    , ("Hello world" + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            // 设置SQL过滤的属性
+            msg.putUserProperty("a", String.valueOf(i));
+            SendResult send = producer.send(msg);
+            System.out.printf("%s%n", send);
+        }
+        producer.shutdown();
+
+    }
+}
